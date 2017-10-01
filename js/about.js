98b9028e0cbe66a2056ca5fdc9d73437dafe1aca
brownac <austincbrown@ufl.edu>
2017-09-30 22:52:07 -0400

    updated site

---
+++ js/about.js
@@ -0,0 +1,41 @@
+$( document ).ready(function() {
+	setTimeout(function() {
+		$("#cam").fadeIn(3000).removeClass("hidden");
+		$(".popout").fadeIn(5000).removeClass("hidden");
+	}, 1000);
+	$('.chips-initial').material_chip({
+	    data: [{
+	      tag: 'Full-stack web development',
+	    }, {
+	      tag: 'UI/UX design',
+	    }, {
+	      tag: 'Big data analytics',
+	    }, {
+	    	tag: 'Systems administration'
+	    }, {
+	    	tag: 'Systems architecture'
+	    }],
+	    autocompleteOptions: {
+	    	data: {
+	    		'HTML': null, 
+	    		'CSS': null, 
+	    		'Javascript': null,
+	    		'Java': null,
+	    		'PHP': null,
+	    		'React': null,
+	    		'Meteor': null,
+	    		'React Native': null,
+	    	},
+	    	limit: Infinity,
+	    	minLength: 1,
+	    },
+	});
+
+});
+
+$("#desc").typeIt({
+	strings: ["a developer","a cat person","a self-proclaimed chef","also a self-proclaimed photographer","an outdoors enthusiast","Austin."],
+	speed: 100,
+	breakLines: false,
+	lifeLike: true,
+});
--- js/main.js
+++ js/main.js
@@ -1,33 +1,3 @@
 $(function() {
-  // navbar controller
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if( $(this).attr("href")=="#myCarousel") return;//This is the exception
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top,
-        }, 1000, 'easeInOutExpo');
-        return false;
-      }
-    }
-  });
-
-  //shows the navbar if scrolled past a certain length
-  $(window).scroll(function() {
-    if ($(".navbar").offset().top > 700) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-        $(".navbar-brand").removeClass("hide").fadeIn(1000);
-        $(".nav.navbar-nav.navbar-right").removeClass("hide").fadeIn(1000);
-    } else if($(".navbar").offset().top == 0) {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-        $(".navbar-brand").addClass("hide").fadeOut(1000);
-        $(".nav.navbar-nav.navbar-right").addClass("hide").fadeIn(1000);
-    }
-  });
-
-  $('.navbar-collapse ul li a').click(function() {
-      $('.navbar-toggle:visible').click();
-  });
+  
 })
